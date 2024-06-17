0d7e6b536f1cbaf65cd793aa4245b5c0a84fb964
Zijie <zijiepang@gmail.com>
2024-06-17 20:30:10 +0800

    add onSubmit function to the sign up page

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -3,7 +3,7 @@ import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
 
 export const signup = async(req, res, next) => {
-    const { userName, email, password} = (req.body);
+    const { userName, email, password} = req.body;
 
     if (!userName || !email || !password || userName === '' || email === '' || password === '') {
         next(errorHandler(400, 'All fields are required'));
@@ -21,7 +21,7 @@ export const signup = async(req, res, next) => {
         await newUser.save();
         res.json("Successful Signup");
     } catch (error) {
-        next(error)
+        next(error);
     }
 
 
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import { Label, TextInput, Button } from 'flowbite-react'
+
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.id]: e.target.value});
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // prevent refreshing the page
+    try {
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+    } catch (error) {}
+  };
   return (
     <div className='min-h-screen mt-20'>
-      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5" >
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-9" >
         {/* left */}
         <div className="flex-1">
           <Link to="/" className='whitespace-nowrap text-4xl font-bold dark:text-white'>
@@ -12,24 +29,24 @@ export default function SignUp() {
               Blog
           </Link>
           <p className='text-sm mt-5'>
-            This is my blog. Feel free to sign up and comment my posts :)
+            This is a place to share my notes and thoughts. Feel free to sign up and comment on my posts :)
           </p>
         </div>
 
         {/* right */}
         <div className="flex-1">
-          <form className='flex flex-col gap-4'>
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
             <div>
               <Label value='Your Username'></Label>
-              <TextInput type='text' placeholder='Username' id='username'></TextInput>
+              <TextInput type='text' placeholder='Username' id='userName' onChange={handleChange}></TextInput>
             </div>
             <div>
               <Label value='Your Email'></Label>
-              <TextInput type='text' placeholder='name@company.com' id='email'></TextInput>
+              <TextInput type='email' placeholder='name@company.com' id='email' onChange={handleChange}></TextInput>
             </div>
             <div>
               <Label value='Your Password'></Label>
-              <TextInput type='text' placeholder='Password' id='password'></TextInput>
+              <TextInput type='password' placeholder='Password' id='password' onChange={handleChange}></TextInput>
             </div>
             <Button gradientDuoTone='purpleToBlue' type='submit'>Sign Up</Button>
           </form>
