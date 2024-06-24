7034e9672972b492e48e36c738d26c471f018873
Zijie <zijiepang@gmail.com>
2024-06-24 11:55:41 +0800

    make the dashboard private

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center" >
         {/* left */}
         <div className="flex-1">
-          <div class="column" className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
+          <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
             <h1 className='text-3xl font-bold lg:text-6xl'>Welcome to my blog</h1>
             <p className='text-gray-500 text-xs sm:txt-sm'>Hi, I'm Mira. Here you'll find a variety 
                   of articles and notes on topics such as computer science and market insights.</p>
