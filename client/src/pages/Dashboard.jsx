bbc4fb2db1a485174cee01fe1e2530c30107aee8
Zijie <zijiepang@gmail.com>
2024-06-24 20:24:09 +0800

    Debug and complete profile page UI

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -15,15 +15,15 @@ export default function Dashboard() {
     }
   }, [location.search]);
   return (
-    <div className="min-h-screen flex flex-col md:flex-row">
-      <div className="md:w-56">
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      <div className='md:w-56'>
         {/* Sidebar */}
         <DashSidebar />
       </div>
-      <div>
-        {/* profile... */}
-        {tab === 'profile' && <DashProfile />}
-      </div>
+      
+      {/* profile... */}
+      {tab === 'profile' && <DashProfile />}
+      
     </div>
   )
 }
