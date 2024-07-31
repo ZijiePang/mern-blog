2cc71152c5bb2eb682004fb04759c1b7d69e9c34
Zijie <zijiepang@gmail.com>
2024-07-31 22:29:15 +0800

    complete create post functionality

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -52,13 +52,38 @@ export default function CreatePost() {
             console.log(error);
         }
     }
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      try {
+        const res = await fetch('/api/post/create', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(formData),
+        });
+        const data = await res.json();
+        console.log(data);
+        if (!res.ok) {
+          setPublishError(data.message);
+          return;
+        }
+  
+        if (res.ok) {
+          setPublishError(null);
+          navigate(`/post/${data.slug}`);
+        }
+      } catch (error) {
+        setPublishError('Something went wrong');
+      }
+    };
   return (
     <div className='p-3 max-w-3xl mx-auto min-h-screen'>
         <h1 className='text-center text-3xl my-7 font-semibold'>Create a post</h1>
-        <form className='flex flex-col gap-4'>
+        <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
             <div className="flex flex-col gap-4 sm:flex-row justify-between">
-                <TextInput type='text' placeholder='Title' required id='title' className='flex-1'></TextInput>
-                <Select>
+                <TextInput type='text' placeholder='Title' required id='title' className='flex-1' onChange={(e)=>setFormData({...formData, title:e.target.value})}></TextInput>
+                <Select onChange={(e)=>setFormData({...formData, category:e.target.value})}>
                     <option value="uncategorized">Select a category</option>
                     <option value="front-end">Front-end</option>
                     <option value="travel">Travel</option>
@@ -83,8 +108,9 @@ export default function CreatePost() {
             {formData.image && (
                 <img src={formData.image} alt='upload' className='w-full h-72 object-cover'></img>
             )}
-            <ReactQuill theme="snow" placeholder="Write something..." className="h-72 mb-12" required></ReactQuill>
+            <ReactQuill theme="snow" placeholder="Write something..." className="h-72 mb-12" required onChange={(value)=>{setFormData({...formData, content:value})}}></ReactQuill>
             <Button type='submit' gradientDuoTone='purpleToBlue'>Publish</Button>
+            {publishError && <Alert className='mt-5' color='failure'>{publishError}</Alert>}
         </form>
     </div>
   )
